40c4204e63f3c792cde031212e6cda2d305db168
ymuab <ymuabhkust@gmail.com>
2019-11-19 17:14:28 +0800

    Made change la

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,6 +3,10 @@
  */
 public class Library {
 	public static void main(String args[]) {
+		Library lib = new Library();
+		if(lib.someLibraryMethod()) {
+			System.out.println("Commit and Push.");
+		}
 		System.out.println("When there is a fire.");
 	}
 	
